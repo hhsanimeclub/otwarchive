377177cbf2d5756491e06db89e2e708ddbcc0267
stampylongr <contact@rororobby.ml>
2020-11-22 15:50:04 +0000

    fix cap deploy stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,4 +11,4 @@ set :keep_releases, 5
 
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/redis.yml'
+append :linked_files, 'config/database.yml', 'config/redis.yml', 'config/local.yml'
